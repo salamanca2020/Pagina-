a3f4d18507b0560ca173463f6e966cd2276c4e07
Mauricio <mawisalamanca@gmail.com>
2021-03-05 18:41:02 -0600

    mi pagina web

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html >
+<html>
+<head>
+  <title></title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"><style type="text/css">
+<!--
+body {
+	background-color: #CCCCCC;
+}
+-->
+</style></head>
+<body>
+<h1> Alumno: Mauricio Salamanca </h1> 
+  <table width="276" height="200" border="1">
+    <tr>
+      <td height="194"><img src="b5d9241053eb0572cf199c43a9ce6c3a.jpg" width="329" height="190"></td>
+    </tr>
+  </table>
+  <h1> Subir archivos </h1>
+  <form action ="subir.php" method ="post" enctype= "multipart/form-data">
+    <input type ="file" name ="archivo">
+    <br><br>
+    <button>subir archivo</button>
+  </form>
+</body>
+  </html>
